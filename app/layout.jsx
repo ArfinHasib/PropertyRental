17d7ad3ba0c2f2e0f95281219a2a34739cd89cbd
ArfinHasib <arfinhasib21375@gmail.com>
2024-07-16 18:14:57 +0600

    Initial Layout

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+    title: "PropertyPulse | Find the Perfect Rental",
+    description: "Find your dream rental property",
+    keyword: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body>
+                <Navbar />
+                <main>{children}</main>
+                <Footer />
+            </body>
+        </html>
+    );
+};
+
+export default MainLayout;
